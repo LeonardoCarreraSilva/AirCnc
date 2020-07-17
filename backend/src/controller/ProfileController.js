584c706ac257d92647ab73c4c0756b194a95ab67
Leonardo Carrera da Silva <leonardosilva.ads@hotmail.com>
2020-07-17 17:54:46 -0300

    Validação feita no ProfileController

--- backend/src/controller/ProfileController.js
+++ backend/src/controller/ProfileController.js
@@ -1,8 +1,14 @@
 const Spots = require('../model/Spot')
-
+const User = require('../model/User')
 module.exports ={
     async show(req, res){
         const { user } = req.headers;
+
+        const user_to_Done = await User.findById(user);
+        if(!user_to_Done){
+            return res.status(400).json({error : "User does not exists!"});
+        }
+
         const   spots = await Spots.find({ user : user });
 
         return res.json(spots);
