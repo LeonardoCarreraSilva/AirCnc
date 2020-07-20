9a677ab362f5baf1cc208da3d042f97ef1f88082
Leonardo Carrera da Silva <leonardosilva.ads@hotmail.com>
2020-07-20 12:34:23 -0300

    Inico do Frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+const { use } = require('./routes');
 const app = express();
 
 mongoose.connect('mongodb+srv://Leo:Leo@aircnc.vnkiz.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -13,6 +15,7 @@ mongoose.connect('mongodb+srv://Leo:Leo@aircnc.vnkiz.mongodb.net/<dbname>?retryW
 //rec.query = Acessar query params (para filtros)
 //rec.params = Acessar route params (para edição, delete)
 //rec.body = Acessar corpo da requisição
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3333);
